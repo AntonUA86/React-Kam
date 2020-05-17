28ad486536f6721da711de1b59fb64b984411639
AntonUA86 <62605227+AntonUA86@users.noreply.github.com>
2020-05-17 13:13:48 +0300

    23

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,29 +2,43 @@ import React from 'react';
 import s from './Dialogs.module.css'
 import { NavLink } from 'react-router-dom';
 
-const Dialogs = () =>{
+
+
+const DialogsItem = (props) => {
+    let path = '/dialogs/' + props.id;
+    return (
+    <div className={s.dialog + ' ' + s.active}>
+        <NavLink to={path}>{props.name}</NavLink>
+    </div>
+    )
+}
+const Message = (props) => {
+    return (
+    <div className={s.message}>{props.message}</div>
+    )
+
+}
+const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-            <div className={s.dialog + ' ' + s.active}>
-                        <NavLink to='/dialogs/1'>Anton</NavLink>
-                </div>
-                <div className={s.dialog}>
-                <NavLink to='/dialogs/2'>Olga</NavLink>
-                </div>
-                <div className={s.dialog}>
-                <NavLink to='/dialogs/3'>Ursa</NavLink>
-                </div>
+            <DialogsItem name="Anton" id="1" />
+            <DialogsItem name="Anton" id="2" />
+            <DialogsItem name="Anton" id="3" />
+            <DialogsItem name="Anton" id="4" />
+            <DialogsItem name="Anton" id="5" />
             </div>
-            
 
-            <div className={s.messages}>
-                <div className={s.message}>Hi</div>
-                <div className={s.message}>Hi Buy</div>
-                <div className={s.message}>Hi Buy Bro</div>
-            </div>
+        <div className={s.messages}>
+            <Message message="Oh, baby, baby"/>
+            <Message message="How was I supposed to know "/>
+            <Message message="That something wasn't right here?"/>
+            <Message message="I shouldn't have let you go"/>
+            <Message message="And now you're out of sight, yeah"/>
+        </div>
         </div>
     )
+    
 }
 
 export default Dialogs;
