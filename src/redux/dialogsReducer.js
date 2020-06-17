74123c0d5b605a80eaa999bf9e60272e15b121a4
Anton K <antonk0095895@gmail.com>
2020-06-17 11:56:49 +0300

    fix:copy state ProfileReducer (47)

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -21,10 +21,13 @@ let initialState = {
 };
 
 const dialogsReducer = (state = initialState, action) => {
+  let stateCopy = { ...state, messages: [...state.messages] };
+
   switch (action.type) {
-    case UPDATE_NEW_MESSAGE_BODY:
+    case UPDATE_NEW_MESSAGE_BODY: {
       state.newMessageBody = action.body;
       return state;
+    }
     case SEND_MESSAGE:
       let body = state.newMessageBody;
       state.newMessageBody = "";
