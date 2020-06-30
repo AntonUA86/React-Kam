e6800ac36b24e77ea7e504e9b29e5691776b0652
Anton K <antonk0095895@gmail.com>
2020-06-30 09:12:49 +0300

    perf:pagination (55)

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 import Users from "./Users";
 import { connect } from "react-redux";
-import { followAC, unfollowAC, setUsersAC } from "../../redux/usersReducer";
+import {
+  followAC,
+  setCurrentPageAC,
+  setUsersAC,
+  setUsersTotalCountAC,
+  unfollowAC,
+} from "../../redux/usersReducer";
 
 let mapStateToProps = (state) => {
   return {
     users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
   };
 };
+
 let mapDispatchToProps = (dispatch) => {
   return {
-    follow: (userID) => {
-      dispatch(followAC(userID));
+    follow: (userId) => {
+      dispatch(followAC(userId));
     },
-    unfollow: (userID) => {
-      dispatch(unfollowAC(userID));
+    unfollow: (userId) => {
+      dispatch(unfollowAC(userId));
     },
     setUsers: (users) => {
       dispatch(setUsersAC(users));
+    },
+    setCurrentPage: (pageNumber) => {
+      dispatch(setCurrentPageAC(pageNumber));
+    },
+    setTotalUsersCount: (totalCount) => {
+      dispatch(setUsersTotalCountAC(totalCount));
     },
   };
 };
