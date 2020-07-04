db7333bb65ec9ec1847a1c184623b2e2f1e23950
Anton K <antonk0095895@gmail.com>
2020-07-04 11:19:51 +0300

    perf:withRouter, props.match.params (60)

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,12 +2,12 @@ import React from "react";
 // import s from './Profile.module.css';
 import MyPostsContainer from "./MyPosts/MyPostContainer";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-// import Preloader from "../Common/Preloader/Preloader";
+import Preloader from "../Common/Preloader/Preloader";
 
 const Profile = (props) => {
-  // if (!props.profile) {
-  //   return <Preloader />;
-  // }
+  if (!props.profile) {
+    return <Preloader />;
+  }
   return (
     <div>
       <ProfileInfo profile={props.profile} />
